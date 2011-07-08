1e0fbe8078c146c8da66fe4b26819a73094b5b04
bdickason <dickason@gmail.com>
2011-07-08 09:52:09 -0400

    Added default variables to config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 // Config.js - Configuration of random stuffs
 
-exports.SESSION_SECRET = process.env.SESSION_SECRET
+exports.SESSION_SECRET = process.env.SESSION_SECRET || "internets";
 
 // Goodreads
-exports.GOODREADS_KEY = process.env.GOODREADS_KEY;
-exports.GOODREADS_SECRET = process.env.GOODREADS_SECRET;
+exports.GOODREADS_KEY = process.env.GOODREADS_KEY || "";
+exports.GOODREADS_SECRET = process.env.GOODREADS_SECRET || "";
 
 // Redis
-exports.REDIS_HOSTNAME = process.env.REDIS_HOSTNAME;
-exports.REDIS_PORT = process.env.REDIS_PORT;
-exports.REDIS_CACHE_TIME = process.env.REDIS_CACHE_TIME;    // Time to cache objects (in seconds)
+exports.REDIS_HOSTNAME = process.env.REDIS_HOSTNAME || "localhost";
+exports.REDIS_PORT = process.env.REDIS_PORT || "6379";
+exports.REDIS_CACHE_TIME = process.env.REDIS_CACHE_TIME || "90";    // Time to cache objects (in seconds)
 
 // Mongo
