6e7d67ee14731c84be256e494c196cc0a932f084
bdickason <dickason@gmail.com>
2011-07-09 14:28:55 -0400

    Converted app.js to coffeescript

---
+++ app.coffee
@@ -0,0 +1,72 @@
+http = require 'http'
+express = require 'express'
+oauth = require 'oauth'
+RedisStore = (require 'connect-redis')(express)
+sys = require 'sys'
+cfg = require './config/config.js'    # contains API keys, etc.
+
+app = express.createServer()
+
+app.configure ->
+  app.set 'views', __dirname + '/views'
+  app.register '.html', require 'jade'
+  app.use express.methodOverride()
+  app.use express.bodyParser()
+  app.use express.cookieParser()
+  app.use express.session { secret: cfg.SESSION_SECRET, store: new RedisStore}
+  app.use app.router
+  
+app.dynamicHelpers { session: (req, res) -> req.session }
+
+### Initialize controllers ###
+Goodreads = new (require './controllers/goodreads.js').Goodreads
+# List = new (require './controllers/list.js').List
+
+### Initialize models
+UsersModel = new (require './models/users').Users
+ListsModel = new (require './models/lists').Lists
+BooksModel = new (require './models/books').Books
+###
+
+### Start Route Handling ###
+
+# Home Page
+app.get '/', (req, res) ->
+  console.log req.session
+  if req.session.goodreads_auth == 1
+    # User is authenticated
+    
+    # Get my shelevs
+    Goodreads.getShelves req.session.goodreads_id, (json) ->
+      if json
+        res.render 'index.jade', { json: json }
+  
+  else
+    # Prompt for login
+    res.send "You are not logged in. <A HREF='/goodreads/connect'>Click here</A> to login"
+
+app.get '/logout', (req, res) ->
+  console.log '--- LOGOUT ---'
+  console.log req.session
+  req.session.destroy()
+  res.redirect '/'
+
+# Goodreads
+app.get '/goodreads/connect', (req, res) ->
+  callback = ''
+  Goodreads.requestToken callback, req, res
+  
+app.get '/goodreads/callback', (req, res) ->
+  # Handle goodreads callback
+  callback = ''
+  Goodreads.callback callback, req, res
+  # Redirect back to '/' when done
+
+app.get '/goodreads/list/:listName', (req, res) ->
+  # Get a specific list
+  Goodreads.getSingleList req.session.goodreads_id, req.params.listName, (json) ->
+    if json
+      # Received valid return from Goodreads
+      res.render 'list.jade', { json: json }
+
+app.listen 3000
--- app.js
+++ app.js
@@ -1,93 +1,75 @@
-var http = require('http');
-var express = require('express');
-var app = express.createServer();
-var oauth = require('oauth');
-var RedisStore = require('connect-redis')(express);
-var sys = require('sys');
-var cfg = require('./config/config.js');    // contains API keys, etc.
-
-app.configure(function(){    
+(function() {
+  var Goodreads, RedisStore, app, cfg, express, http, oauth, sys;
+  http = require('http');
+  express = require('express');
+  oauth = require('oauth');
+  RedisStore = (require('connect-redis'))(express);
+  sys = require('sys');
+  cfg = require('./config/config.js');
+  app = express.createServer();
+  app.configure(function() {
     app.set('views', __dirname + '/views');
     app.register('.html', require('jade'));
     app.use(express.methodOverride());
     app.use(express.bodyParser());
     app.use(express.cookieParser());
-    app.use(express.session({ secret: cfg.SESSION_SECRET, store: new RedisStore}));
-    app.use(app.router); 
-});
-
-app.dynamicHelpers({
-  session: function(req, res){
-    return req.session;
-  }
-});
-
-// Initialize controllers
-// var List = new (require('./controllers/list.js').List);                  // List display, etc
-var Goodreads = new (require('./controllers/goodreads.js').Goodreads);   // Goodreads connectivity
-
-
-// Initialize models
-var UsersModel = new (require('./models/users').Users);
-var ListsModel = new (require('./models/lists').Lists);
-var BooksModel = new (require('./models/books').Books);
-
-// Start route handling
-
-// Home Page
-app.get('/', function(req, res) {
-    
+    app.use(express.session({
+      secret: cfg.SESSION_SECRET,
+      store: new RedisStore
+    }));
+    return app.use(app.router);
+  });
+  app.dynamicHelpers({
+    session: function(req, res) {
+      return req.session;
+    }
+  });
+  /* Initialize controllers */
+  Goodreads = new (require('./controllers/goodreads.js')).Goodreads;
+  /* Initialize models
+  UsersModel = new (require './models/users').Users
+  ListsModel = new (require './models/lists').Lists
+  BooksModel = new (require './models/books').Books
+  */
+  /* Start Route Handling */
+  app.get('/', function(req, res) {
     console.log(req.session);
-    if(req.session.goodreads_auth == 1) {
-        // User is authenticated
-        
-        // Get my shelves
-        Goodreads.getShelves(req.session.goodreads_id, function(json) {
-    	    if(json)
-    	    {
-    	        // Received valid return from Goodreads
-                    res.render("index.jade", { json: json });
-    	    } 
-    	});
+    if (req.session.goodreads_auth === 1) {
+      return Goodreads.getShelves(req.session.goodreads_id, function(json) {
+        if (json) {
+          return res.render('index.jade', {
+            json: json
+          });
+        }
+      });
+    } else {
+      return res.send("You are not logged in. <A HREF='/goodreads/connect'>Click here</A> to login");
     }
-    else
-    {
-        // Prompt for login
-        res.send("You are not logged in. <A HREF='/goodreads/connect'>Click here</A> to login");
-    }
-});
-
-app.get('/logout', function(req, res) {
-    console.log("--- LOGOUT ---")
+  });
+  app.get('/logout', function(req, res) {
+    console.log('--- LOGOUT ---');
     console.log(req.session);
     req.session.destroy();
-    res.redirect("/");
-});
-
-
-/* GOODREADS */
-
-app.get('/goodreads/connect', function(req, res) {
-    var callback = "";
-    Goodreads.requestToken(callback, req, res);
-});
-
-app.get('/goodreads/callback', function(req, res) {
-   // Handle goodreads callback
-   // Redirect to /goodreads 
-   var callback = "";
-      Goodreads.callback(callback, req, res);    
-});
-
-app.get('/goodreads/list/:listName', function(req, res) {
-    // Get a specific list
-	Goodreads.getSingleList(req.session.goodreads_id, req.params.listName, function(json) {	    
-	    if(json) {
-	        // Received valid return from Goodreads
-		    res.render("list.jade", { json: json });
-	    }
-	}); 
-});
-
-
-app.listen(3000);
+    return res.redirect('/');
+  });
+  app.get('/goodreads/connect', function(req, res) {
+    var callback;
+    callback = '';
+    return Goodreads.requestToken(callback, req, res);
+  });
+  app.get('/goodreads/callback', function(req, res) {
+    var callback;
+    callback = '';
+    return Goodreads.callback(callback, req, res);
+  });
+  app.get('/goodreads/list/:listName', function(req, res) {
+    return Goodreads.getSingleList(req.session.goodreads_id, req.params.listName, function(json) {
+      if (json) {
+        return res.render('list.jade', {
+          json: json
+        });
+      }
+    });
+  });
+  app.listen(3000);
+}).call(this);
